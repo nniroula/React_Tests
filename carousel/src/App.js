f26a21426ef832f3bb644441c55670053c3292fc
nabin <nkbhaie2017@gmail.com>
2022-08-05 17:48:05 -0600

    initial commit

---
+++ carousel/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Carousel from "./Carousel";
+
+// This the top level component that renders other components in React
+function App() {
+  return (
+    <div className="App">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
